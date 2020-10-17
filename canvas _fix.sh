89de7ee123b1ac65f4c31bb094e46d7a4298378b
Bảo <55090875+Corey-Stowe@users.noreply.github.com>
2020-10-17 16:33:30 +0700

    Add files via upload

---
+++ canvas _fix.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+echo "----------------Canvas fix---------------------------------"
+echo "Dựa Theo Bản Gốc trên C3C bot trên Github"
+echo "Leader source: Lê Quang lâm (URI)"
+echo "Admin : Nguyễn Hải Anh (Kidchoi), Trương Gia Hy, Nguyễn Minh Hiếu,...."
+echo "Setup bởi Stowe(Edd)"
+echo "+Canvas fix"
+echo "v 0.5"
+echo "----------------------------------------------------------"
+echo "Tiến hành cài sau 5s..."
+sleep 5
+sudo apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+echo "đã cài xong tự hủy sau 5s"
+sleep 5
+echo "đã tự hủy thành công."
